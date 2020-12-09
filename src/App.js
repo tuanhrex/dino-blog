29441ea19d80774cc66ddf7dcf8551eb5f74e22e
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-12-09 12:45:01 -0500

    [dino] Finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
-import Dinosaur from './Dinosaurs';
 import Dinosaurs from './Dinosaurs'
 
 function App(props) {
   
   return (
     <div className="App">
-      {/* <Dinosaurs title={dinosaur.title} author={dinosaur.author} body={dinosaur.body} comments={dinosaur.comments}/> */}
-      <h1> {props.post.title}</h1>
-      <p>by {props.post.author}</p>
-      <p>{props.post.body}</p>
-      <h2>Comments:</h2>
-      <p>{props.post.comments[0]}</p>
+      
+      <Dinosaurs 
+        title={props.post.title}
+        author= {props.post.author}
+        body={props.post.body}
+        comments={props.post.comments}
+      />
+
     </div>
   );
 }
--- src/Dinosaurs.js
+++ src/Dinosaurs.js
@@ -1,13 +1,54 @@
-function Dinosaur(props) {
-    return (
-        <div>
-            <h1> {props.title}</h1>
-            <p>by {props.author}</p>
-            <p>{props.body}</p>
-            <h2>Comments:</h2>
-            <p>{props.comments[0]}</p>
-        </div>
-    )
+// function Dinosaurs(props) {
+//     return (
+        // <div>
+        //     <h1>Title: {props.title}</h1>
+        //     <p>Author: {props.author}</p>
+        //     <p>Body: {props.body}</p>
+        //     <h2>Comments</h2>
+            
+        // </div>
+//     )
+// }
+
+import React, { Component } from 'react'
+
+class Dinosaurs extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          
+          body: this.props.body,
+        
+          input: ''
+        }
+       
+    }
+    change = () => {
+        this.setState({ body: this.state.input })
+    }
+
+
+
+    changeInput = (evt) => {
+        
+        this.setState({input: evt.target.value})
+      }  
+
+    
+    render() {
+        return (
+            <div>
+            <h1>Title: {this.props.title}</h1>
+            <p>Author: {this.props.author}</p>
+            <p>Body: {this.state.body}</p>
+            <input value ={this.state.input} onChange={this.changeInput}/>
+            <button onClick={this.change}>Edit</button>
+            <h2>Comments</h2>
+            <p>{this.props.comments[0]}</p>
+            
+            </div>
+        );
+    }
 }
 
-export default Dinosaur
+export default Dinosaurs
